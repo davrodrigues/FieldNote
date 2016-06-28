91142f922d80f4d48505ebd8db3126d53cee6ffd
apcr12 <ap.roque@campus.fct.unl.pt>
2016-06-28 13:19:06 +0100

    alterei a main

--- app/src/main/java/com/example/diogo/fieldnote/LoginActivity.java
+++ app/src/main/java/com/example/diogo/fieldnote/LoginActivity.java
@@ -43,6 +43,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
+    // antonio
+
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
